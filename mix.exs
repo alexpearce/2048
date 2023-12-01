da315111abe64117e28a8d86b39c37bd42d6960d
Alex Pearwin <alex@pearwin.com>
2023-12-01 16:30:49 +0000

    Add test watcher.

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule TwentyFortyEight.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
